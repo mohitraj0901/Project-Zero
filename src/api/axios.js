75671540d402bcfd1cd7f4be316c648400292205
mohitraj0401 <mohit.2023ug3001@iiitranchi.ac.in>
2026-02-21 17:05:35 +0530

    updated base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL:  "https://project-zero-1a6h.onrender.com/",
 });
 
 // Automatically attach token if exists
